778b4d766a48122c44b8529219c9e5d9ced8ab9a
devkavin <kavindra.senanayake@gmail.com>
2026-07-20 14:50:10 +0530

    refactor: update database connection configuration and clean up docker-compose

--- backend/src/database/pool.ts
+++ backend/src/database/pool.ts
@@ -2,15 +2,26 @@ import mysql from "mysql2/promise";
 import { env } from "../config/env.js";
 
 export const pool = mysql.createPool({
-  host: env.DB_HOST,
-  port: env.DB_PORT,
-  database: env.DB_NAME,
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
+   uri: process.env.DATABASE_URL,
+  // host: process.env.DB_HOST,
+  // port: Number(process.env.DB_PORT || 3306),
+  // database: process.env.DB_NAME,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+
+  ssl:
+  process.env.DB_SSL === "true"
+    ? {
+        rejectUnauthorized: false,
+      }
+    : undefined,
+
   timezone: env.DB_TIMEZONE,
   waitForConnections: true,
   connectionLimit: 10,
-  namedPlaceholders: true
+  namedPlaceholders: true,
+  queueLimit: 0,
+
 });
 
 export type Db = typeof pool;
